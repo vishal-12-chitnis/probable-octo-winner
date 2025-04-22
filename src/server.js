ccea1beed3d65c0ddb39b9c5b2b88bde5b0bef96
vishal-12-chitnis <vishal.chitnis@checkmarx.com>
2025-04-22 14:36:56 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,11 @@ app.get("/0", (req, res) => {
   res.send("Hello world!");
 });
 
+app.get("/1", (req, res) => {
+  eval(req.body.command1);
+  res.send("Hello world!");
+}
+
 app.listen(port, () => {
   console.log(`Example app listening on port  ${port}`);
 });
